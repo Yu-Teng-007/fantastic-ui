cbe0942578b3d119e737d486b33c435aa8772421
JinCheng_77 <JinCheng_77.onaliyun.com>
2025-05-10 10:42:46 +0800

    feat：创建_variables.scss文件，专门用来存放css变量，这些变量涵盖了颜色、间距、字体、边框、阴影以及特定组件（如按钮、弹窗、单元格、提示、日历、表单和时间选择器）的样式。创建components文件夹用来存放组件

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// This is the entry point for your component library.
+// You will export your components from here.
+
+// Example:
+// import MyButton from './components/MyButton.vue';
+// export { MyButton };
+
+export default {};
