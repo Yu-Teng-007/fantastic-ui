fd35450caa967a22ead516c63d51e796b11df3a8
JinCheng_77 <JinCheng_77.onaliyun.com>
2025-05-10 11:43:45 +0800

    feat：添加了首页跳转和按钮示例页面路由

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,49 +1,75 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view>
-			<text class="title">{{title}}</text>
-		</view>
-	</view>
+    <view class="content">
+        <image class="logo" src="/static/logo.png"></image>
+        <view>
+            <text class="title">{{ title }}</text>
+        </view>
+
+        <view class="nav-section">
+            <navigator url="/pages/button-example/button-example" class="nav-button">
+                <text class="nav-text">查看按钮示例</text>
+            </navigator>
+        </view>
+    </view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
-
-		},
-		methods: {
-
-		}
-	}
+export default {
+    data() {
+        return {
+            title: "Hello",
+        };
+    },
+    onLoad() {},
+    methods: {},
+};
 </script>
 
 <style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
+.content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
 
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin: 200rpx auto 50rpx auto;
-	}
+.logo {
+    height: 200rpx;
+    width: 200rpx;
+    margin: 200rpx auto 50rpx auto;
+}
 
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
+.text-area {
+    display: flex;
+    justify-content: center;
+}
 
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
+.title {
+    font-size: 36rpx;
+    color: #8f8f94;
+}
+
+.nav-section {
+    margin-top: 50rpx;
+    display: flex;
+    justify-content: center;
+}
+
+.nav-button {
+    background-color: #007aff;
+    color: white;
+    padding: 20rpx 40rpx;
+    border-radius: 10rpx;
+    text-align: center;
+    box-shadow: 0 4rpx 8rpx rgba(0, 0, 0, 0.1);
+    transition: background-color 0.3s;
+}
+
+.nav-button:hover {
+    background-color: #0056b3;
+}
+
+.nav-text {
+    font-size: 32rpx;
+}
 </style>
